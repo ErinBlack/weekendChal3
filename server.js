4db080f644779ca843c2ea4adbb2a77ffafec6b9
Erin Black <erinblackdesign@gmail.com>
2017-06-03 11:29:56 -0500

    created route to load index page

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 //require
 var express = require('express');
 var app = express();
+var index = require('./routes/index');
 var path = require('path');
 var bodyParser = require('body-parser');
 var pg = require('pg');
 var port = 8080;
+var addTask = require('./routes/addTask');
+
 
 
 //use
 app.use(express.static('public'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', index);
 
 //listen
 app.listen(port, function(){
   console.log('server up on 8080');
 });
 //get index.html on the page
-app.get('/', function(req,res){
-  console.log('url hit on /');
-  res.sendFile(path.resolve('public/views/index.html'));
-}); // end get / get request 
