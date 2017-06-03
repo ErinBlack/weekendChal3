c91a871aa7ea3e4196e52eb29fa5e9fc853c05e6
Erin Black <erinblackdesign@gmail.com>
2017-06-03 13:35:32 -0500

    got list of tasks on page load

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,16 +1,23 @@
-$(onReady);
-function onReady(){
+$(document).ready(function(){
 
-// displayTasks();
-}//end onReady
+displayTasks();
+});//end onReady
 
-
-// function displayTasks(){
-//   $.ajax({
-//     type: 'GET',
-//     url: '/allTasks',
-//     success:function(response){
-//     console.log('back from server', response);
-//     }
-//   });
-// }
+function displayTasks(){
+  console.log('displayTasks entered');
+  $.ajax({
+    type: 'GET',
+    url: '/allTasks',
+    success:function(response){
+    console.log('back from server', response);
+    for (var i = 0; i < response.length; i++) {
+        $('.taskList').append('<li>' + response[i].task +
+        '<button type="button" name="remove" class="deleteTask" id="' +
+        response[i].id + '">Delete</button></li>');
+    }
+  },
+  error: function(error){
+    console.log('The /allTasks ajax get request failed with error: ', error);
+  }
+  });
+}
--- server.js
+++ server.js
@@ -3,12 +3,21 @@ var express = require('express');
 var app = express();
 var index = require('./routes/index');
 var addTask = require('./routes/addTask');
-var getTasks = require('./routes/getTasks')
+var getTasks = require('./routes/getTasks');
 var path = require('path');
 var bodyParser = require('body-parser');
 var pg = require('pg');
 var port = 8080;
 
+//configure the database setup
+var config = {
+  database: 'to_do_list',
+  host: 'localhost',
+  port: 5432,
+  max: 50
+};
+//database pool
+var pool = new pg.Pool(config);
 
 
 
@@ -19,10 +28,41 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', index);
 app.get('/addTask', addTask);
-app.get('getTasks', getTasks);
+app.get('/getTasks', getTasks);
 
 //listen
 app.listen(port, function(){
   console.log('server up on 8080');
 });
 //get index.html on the page
+
+
+//getting all tasks from the database
+app.get('/allTasks', function(req, res){
+  console.log('executed get tasks');
+  var allTasks = [];
+  //open up connection to database
+  pool.connect(function(err, connection, done){
+    if(err){
+      console.log(err);
+      done();
+      res.send(400);
+    }//end if statement
+    else{
+      console.log('connected to db');
+      var resultSet = connection.query("SELECT * FROM things_to_do");
+      resultSet.on('row', function(row){
+        allTasks.push(row);
+        console.log(allTasks);
+
+      });// end function
+      resultSet.on('end',function(){
+        done();
+        console.log(resultSet);
+        res.send(allTasks);
+      }); //end function
+    } // end else statement
+  }); // end database connection
+
+
+}); // end get
