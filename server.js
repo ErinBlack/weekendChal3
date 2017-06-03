c03814133e333514cac1f7ee41cf6d5a11a80da8
Erin Black <erinblackdesign@gmail.com>
2017-06-03 11:16:07 -0500

    created file framework for application and server is running

---
+++ server.js
@@ -0,0 +1,22 @@
+//require
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var pg = require('pg');
+var port = 8080;
+
+
+//use
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//listen
+app.listen(port, function(){
+  console.log('server up on 8080');
+});
+
+app.get('/', function(req,res){
+  console.log('url hit on /');
+  res.sendFile(path.resolve('public/view/index.html'));
+});
